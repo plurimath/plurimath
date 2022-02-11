cdb326193a3b8684e7f31bdf52c3114b2f54e32e
suleman-uzair <suleman@workytical.com>
2022-02-11 19:39:06 +0500

    structure updates

--- lib/plurimath/asciimath_parser.rb
+++ lib/plurimath/asciimath_parser.rb
@@ -3,7 +3,6 @@
 require_relative 'asciimath_parser/sin'
 require_relative 'asciimath_parser/tan'
 require_relative 'asciimath_parser/cos'
-require_relative 'asciimath_parser/theta'
 require_relative 'asciimath_parser/number'
 require_relative 'asciimath_parser/symbol'
 require_relative 'asciimath_parser/variable'
