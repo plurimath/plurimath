9d6bfbe4bd015eb3570abc74a08f9e3d48098c7a
suleman-uzair <suleman@workytical.com>
2022-04-22 16:07:32 +0500

    testing constants file

---
+++ lib/plurimath/mathml/transform.rb
@@ -0,0 +1,195 @@
+# frozen_string_literal: true
+
+module Plurimath
+  class Mathml
+    class Transform < Parslet::Transform
+      rule(tag: simple(:tag))       { tag }
+      rule(tag: sequence(:tag))     { tag }
+      rule(text: simple(:text))     { Plurimath::Math::Symbol.new(text.to_s) }
+      rule(class: simple(:string))  { Transform.get_class(string).new }
+      rule(number: simple(:number)) { Plurimath::Math::Number.new(number.to_s) }
+      rule(tag: sequence(:tag), sequence: simple(:sequence)) { tag + [sequence] }
+      rule(tag: simple(:tag), sequence: sequence(:sequence)) { [tag] + sequence }
+      rule(tag: sequence(:tag), sequence: sequence(:sequence)) { tag + sequence }
+
+      rule(quoted_text: simple(:quoted_text)) do
+        text = quoted_text.to_s
+        Constants::UNICODE_SYMBOLS.each do |code, string|
+          text.gsub!(code.to_s, "unicode[:#{string}]")
+        end
+        Plurimath::Math::Function::Text.new(text)
+      end
+
+      rule(
+        tag: sequence(:tag),
+        sequence: sequence(:sequence),
+        iteration: simple(:iteration),
+      ) do
+        new_arr = []
+        new_arr = new_arr + tag unless tag.compact.empty?
+        new_arr = new_arr + sequence unless sequence.compact.empty?
+        new_arr << iteration unless iteration.to_s.empty?
+        new_arr
+      end
+
+      rule(
+        tag: sequence(:tag),
+        sequence: simple(:sequence),
+        iteration: simple(:iteration),
+      ) do
+        Plurimath::Math::Formula.new(tag + [sequence])
+      end
+
+      rule(
+        tag: simple(:tag),
+        sequence: simple(:sequence),
+        iteration: simple(:iteration),
+      ) do
+        [tag, sequence]
+      end
+
+      rule(tag: sequence(:tag), iteration: simple(:iteration)) do
+        new_arr = []
+        new_arr = tag unless tag.compact.empty?
+        new_arr << iteration unless iteration.to_s.empty?
+        new_arr
+      end
+
+      rule(tag: simple(:tag), sequence: simple(:sequence)) do
+        new_arr = []
+        new_arr << tag unless tag.nil?
+        new_arr << sequence unless sequence.nil?
+        new_arr
+      end
+
+      rule(tag: simple(:tag), iteration: simple(:iteration)) do
+        new_arr = []
+        new_arr << tag unless tag.to_s.empty?
+        new_arr << iteration unless iteration.to_s.empty?
+        new_arr
+      end
+
+      rule(symbol: simple(:symbol)) do
+        decoded_symbol = Constants::UNICODE_SYMBOLS[symbol.to_sym]
+        if Constants::CLASSES.include?(decoded_symbol)
+          Transform.get_class(decoded_symbol).new
+        elsif decoded_symbol.nil? && Constants::SYMBOLS[symbol.to_sym]
+          Plurimath::Math::Symbol.new(Constants::SYMBOLS[symbol.to_sym])
+        else
+          Plurimath::Math::Symbol.new(decoded_symbol)
+        end
+      end
+
+      rule(name: simple(:name), value: simple(:value)) do
+        if ["open", "close"].include?(name.to_s)
+          Plurimath::Math::Symbol.new(value.to_s)
+        elsif name.to_s == "mathcolor"
+          Plurimath::Math::Function::Color.new(value.to_s)
+        elsif name.to_s == "mathvariant"
+          value.to_s
+        end
+      end
+
+      rule(
+        tag: simple(:tag),
+        sequence: sequence(:sequence),
+        iteration: simple(:iteration),
+      ) do
+        tag_str = tag.to_s.split("::").last.downcase.to_sym
+        if Constants::BINARY_CLASSES.include?(tag_str)
+          tag.new(sequence.first, sequence.last)
+        else
+          new_arr = sequence.compact
+          new_arr = [tag] + new_arr unless tag.nil?
+          new_arr << iteration unless iteration.to_s.empty?
+          new_arr
+        end
+      end
+
+      rule(
+        open: simple(:open_tag),
+        attributes: sequence(:attributes),
+        iteration: sequence(:iteration),
+        close: simple(:close_tag),
+      ) do
+        Transform.raise_error!(open_tag, close_tag) unless open_tag == close_tag
+
+        if open_tag == "mrow"
+          Plurimath::Math::Formula.new(iteration)
+        elsif open_tag == "munder"
+          if Transform.get_class("obrace") == iteration.last.class
+            iteration.last.parameter_one = iteration.first
+            iteration.last
+          else
+            Plurimath::Math::Function::Underset.new(iteration[1], iteration[0])
+          end
+        elsif open_tag == "munderover"
+          Plurimath::Math::Function::Underover.new(iteration[0], iteration[1], iteration[2])
+        elsif open_tag == "mover"
+          if Transform.get_class("ubrace") == iteration.last.class
+            iteration.last.parameter_one = iteration.first
+            iteration.last
+          else
+            Plurimath::Math::Function::Overset.new(iteration[1], iteration[0])
+          end
+        elsif open_tag == "msup"
+          Plurimath::Math::Function::Power.new(iteration[0], iteration[1])
+        elsif open_tag == "msub"
+          Plurimath::Math::Function::Base.new(iteration[0], iteration[1])
+        elsif open_tag == "msubsup"
+          Plurimath::Math::Function::PowerBase.new(iteration[0], iteration[1], iteration[2])
+        elsif open_tag == "mfrac"
+          Plurimath::Math::Function::Frac.new(iteration.first, iteration.last)
+        elsif open_tag == "msqrt"
+          Plurimath::Math::Function::Sqrt.new(iteration.first)
+        elsif open_tag == "mroot"
+          Plurimath::Math::Function::Root.new(iteration[0], iteration[1])
+        elsif open_tag == "mfenced"
+          Plurimath::Math::Function::Fenced.new(attributes[0], iteration, attributes[1])
+        elsif open_tag == "mtable"
+          Plurimath::Math::Function::Table.new(iteration)
+        elsif open_tag == "mtr"
+          Plurimath::Math::Function::Tr.new(iteration)
+        elsif open_tag == "mtd"
+          Plurimath::Math::Function::Td.new(iteration)
+        elsif attributes.first.is_a?(Plurimath::Math::Function::Color)
+          attributes.first.parameter_two = iteration.first
+          attributes.first
+        elsif open_tag == "mstyle" && !attributes.compact.empty?
+          if Constants::FONT_CLASSES.key?(attributes.compact.last.to_sym)
+            math_class = Constants::FONT_CLASSES[attributes.compact.last.to_sym]
+            math_class.new(iteration.last)
+          end
+        else
+          iteration
+        end
+      end
+
+      rule(
+        open: simple(:open_tag),
+        attributes: sequence(:attributes),
+        iteration: simple(:iteration),
+        close: simple(:close_tag),
+      ) do
+        Transform.raise_error!(open_tag, close_tag) unless open_tag == close_tag
+
+        if iteration.to_s.include?("Function")
+          iteration
+        else
+          [iteration.to_s.empty? ? nil : iteration]
+        end
+      end
+
+      def self.get_class(text)
+        Object.const_get("Plurimath::Math::Function::#{text.to_s.capitalize}")
+      end
+
+      def self.raise_error!(open_tag, close_tag)
+        message = "Please check your input."\
+                  " Opening tag is \"#{open_tag}\""\
+                  " and closing tag is \"#{close_tag}\""
+        raise Plurimath::Math::Error.new(message)
+      end
+    end
+  end
+end
