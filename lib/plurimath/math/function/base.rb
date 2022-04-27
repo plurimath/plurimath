f9480a3b5e3467b662c17d76647a6aefa5a0bc5e
suleman-uzair <suleman@workytical.com>
2022-04-27 11:54:27 +0500

    fixed DRY comment and updated code

--- lib/plurimath/math/function/base.rb
+++ lib/plurimath/math/function/base.rb
@@ -6,6 +6,11 @@ module Plurimath
   module Math
     module Function
       class Base < BinaryFunction
+        def to_asciimath
+          first_value = "#{parameter_one.to_asciimath}" if parameter_one
+          second_value = "_#{parameter_two.to_asciimath}" if parameter_two
+          "#{first_value}#{second_value}"
+        end
       end
     end
   end
