c9ce4e1f062e66f5cd1d51560b618278de24de7a
suleman-uzair <suleman@workytical.com>
2022-05-27 13:18:33 +0500

    fixed failing spec's

--- lib/plurimath/math.rb
+++ lib/plurimath/math.rb
@@ -13,6 +13,7 @@ require_relative "math/number"
 require_relative "math/symbol"
 require_relative "asciimath/parser"
 require_relative "mathml/parser"
+require_relative "latex/parser"
 module Plurimath
   module Math
     class Error < StandardError; end
