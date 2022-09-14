1466d9b075ade0e105703d40b8bfd71b0928d3ba
suleman-uzair <suleman@techieminions.co>
2022-09-14 20:11:10 +0500

    updated constants, parser rules and fixture omml files

--- spec/plurimath/omml/parse_spec.rb
+++ spec/plurimath/omml/parse_spec.rb
@@ -123,4 +123,346 @@ RSpec.describe Plurimath::Omml::Parse do
       expect(formula[:close]).to eq("oMathPara")
     end
   end
+
+  context "contains #008.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/008.omml" }
+
+    it "matches open and close tag" do
+      spre_tag = formula[:iteration][:iteration]
+
+      expect(spre_tag[:open]).to eq("sPre")
+      expect(spre_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(spre_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(spre_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(spre_tag[:close]).to eq("sPre")
+    end
+  end
+
+  context "contains #009.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/009.omml" }
+
+    it "matches open and close tag" do
+      sqrt_tag = formula[:iteration][:iteration]
+
+      expect(sqrt_tag[:open]).to eq("rad")
+      expect(sqrt_tag[:fonts][:open]).to eq("radPr")
+      expect(sqrt_tag[:fonts][:close]).to eq("radPr")
+      expect(sqrt_tag[:first_value][:omission]).to eq("deg")
+      expect(sqrt_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(sqrt_tag[:close]).to eq("rad")
+    end
+  end
+
+  context "contains #010.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/010.omml" }
+
+    it "matches open and close tag" do
+      sqrt_tag = formula[:iteration][:iteration]
+
+      expect(sqrt_tag[:open]).to eq("rad")
+      expect(sqrt_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(sqrt_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(sqrt_tag[:close]).to eq("rad")
+    end
+  end
+
+  context "contains #011.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/011.omml" }
+
+    it "matches open and close tag" do
+      sqrt_tag = formula[:iteration][:iteration]
+
+      expect(sqrt_tag[:open]).to eq("rad")
+      expect(sqrt_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(sqrt_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(sqrt_tag[:close]).to eq("rad")
+    end
+  end
+
+  context "contains #012.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/012.omml" }
+
+    it "matches open and close tag" do
+      sqrt_tag = formula[:iteration][:iteration]
+
+      expect(sqrt_tag[:open]).to eq("rad")
+      expect(sqrt_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(sqrt_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(sqrt_tag[:close]).to eq("rad")
+    end
+  end
+
+  context "contains #013.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/013.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:omission]).to eq("sub")
+      expect(nary_tag[:second_value][:omission]).to eq("sup")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #014.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/014.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #015.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/015.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #016.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/016.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:omission]).to eq("sub")
+      expect(nary_tag[:second_value][:omission]).to eq("sup")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #017.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/017.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #018.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/018.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #019.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/019.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:omission]).to eq("sub")
+      expect(nary_tag[:second_value][:omission]).to eq("sup")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #020.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/020.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #021.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/021.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #022.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/022.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:omission]).to eq("sub")
+      expect(nary_tag[:second_value][:omission]).to eq("sup")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #023.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/023.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #024.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/024.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #025.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/025.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:omission]).to eq("sub")
+      expect(nary_tag[:second_value][:omission]).to eq("sup")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #026.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/026.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #027.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/027.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #028.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/028.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:omission]).to eq("sub")
+      expect(nary_tag[:second_value][:omission]).to eq("sup")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #029.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/029.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #030.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/030.omml" }
+
+    it "matches open and close tag" do
+      nary_tag = formula[:iteration][:iteration]
+
+      expect(nary_tag[:open]).to eq("nary")
+      expect(nary_tag[:first_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("1")
+      expect(nary_tag[:second_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("2")
+      expect(nary_tag[:third_value][:iteration][:iteration][:sequence][:iteration][:number]).to eq("3")
+      expect(nary_tag[:close]).to eq("nary")
+    end
+  end
+
+  context "contains #031.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/031.omml" }
+
+    it "matches open and close tag" do
+      iteration = formula[:iteration][:iteration][:iteration][:sequence][:iteration]
+
+      expect(iteration[:iteration][:sequence][:iteration][0][:text]).to eq("d")
+      expect(iteration[:iteration][:sequence][:iteration][1][:text]).to eq("x")
+    end
+  end
+
+  context "contains #032.omml" do
+    let(:file_name) { "spec/plurimath/fixtures/032.omml" }
+
+    it "matches open and close tag" do
+      iteration = formula[:iteration][:iteration][:iteration][:sequence][:iteration]
+
+      expect(iteration[:iteration][:sequence][:iteration][0][:text]).to eq("d")
+      expect(iteration[:iteration][:sequence][:iteration][1][:text]).to eq("y")
+    end
+  end
 end
