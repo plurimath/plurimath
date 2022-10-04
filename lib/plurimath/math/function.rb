e2dcc4e734d9e11f1847de2ef1c588d7dc8604f6
Ronald Tse <ronald.tse@ribose.com>
2022-10-04 13:18:49 +0800

    fix: require issues with function.rb, fixes #54

--- lib/plurimath/math/function.rb
+++ lib/plurimath/math/function.rb
@@ -1,3 +1,14 @@
-Dir[File.join(__dir__, "function/**/*.rb")].each do |file|
-  require_relative file.gsub(".rb", "")
+module Plurimath
+  module Math
+    module Function
+    end
+  end
+end
+
+# Include the first level files before the next
+(
+  Dir.glob(File.join(__dir__, "function", "*.rb")) +
+  Dir.glob(File.join(__dir__, "function", "*", "*.rb"))
+).each do |file|
+  require file
 end
