5a64e6bc7c4d8b8f6101796422bd5098655d1941
suleman-uzair <suleman@workytical.com>
2023-04-18 18:35:38 +0500

    Added exception spec

--- spec/plurimath/math_spec.rb
+++ spec/plurimath/math_spec.rb
@@ -1,8 +1,28 @@
 require_relative '../../lib/plurimath/math'
 
 RSpec.describe Plurimath::Math do
+  describe ".initialize" do
+    subject(:formula) { Plurimath::Math.parse(input, type: type) }
 
-  it "raises error on wrong type" do
-    expect{Plurimath::Math.parse("asdf", type: 'wrong_type')}.to raise_error(Plurimath::Math::Error)
+    context "contains incorrect type with parseable input of asciimath" do
+      let(:input) { "sin(d)" }
+      let(:type) { "wrong_type" }
+
+      it "raises error on wrong type" do
+        expect{formula}.to raise_error(Plurimath::Math::Error)
+      end
+    end
+
+    context "contains correct type with incomplete input of latex" do
+      let(:input) { "{\\sin{d}" }
+      let(:type) { "latex" }
+
+      it "raises error on wrong text input" do
+        error_message = "An error occurred while processing the input. "\
+                        "Please check your input to ensure it is valid "\
+                        "or open an issue on Github If you believe the input is correct."
+        expect{formula}.to raise_error(Plurimath::Math::Error, Regexp.compile(error_message))
+      end
+    end
   end
 end
