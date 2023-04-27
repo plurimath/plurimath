4f804d431e4b6d207dbba16b991b1285bf59a1b2
Ronald Tse <ronald.tse@ribose.com>
2023-04-27 11:53:18 +0800

    chore: differentiate ParseError and InvalidTypeError

--- spec/plurimath/math_spec.rb
+++ spec/plurimath/math_spec.rb
@@ -2,26 +2,26 @@ require_relative '../../lib/plurimath/math'
 
 RSpec.describe Plurimath::Math do
   describe ".initialize" do
-    subject(:formula) { Plurimath::Math.parse(input, type: type) }
+    subject(:formula) { Plurimath::Math.parse(input, type) }
 
     context "contains incorrect type with parseable input of asciimath" do
       let(:input) { "sin(d)" }
       let(:type) { "wrong_type" }
 
       it "raises error on wrong type" do
-        expect{formula}.to raise_error(Plurimath::Math::Error)
+        expect{formula}.to raise_error(Plurimath::Math::InvalidTypeError)
       end
     end
 
     context "contains correct type with incomplete input of latex" do
-      let(:input) { "{\\sin{d}" }
+      let(:input) { '{\sin{d}' }
       let(:type) { "latex" }
 
       it "raises error on wrong text input" do
-        error_message = "An error occurred while processing the input. "\
-                        "Please check your input to ensure it is valid "\
-                        "or open an issue on Github If you believe the input is correct."
-        expect{formula}.to raise_error(Plurimath::Math::Error, Regexp.compile(error_message))
+        message = "Failed to parse the following formula with type `#{type}`"
+        expect{formula}.to raise_error(
+          Plurimath::Math::ParseError, Regexp.compile(message)
+        )
       end
     end
   end
