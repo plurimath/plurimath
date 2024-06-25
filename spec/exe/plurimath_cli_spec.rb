1926b3815dacf7d50cdec40de8798b327bd04fca
suleman-uzair <suleman@workytical.com>
2024-06-25 17:44:55 +0500

    Added cli arguments and specs for convert

---
+++ spec/exe/plurimath_cli_spec.rb
@@ -0,0 +1,77 @@
+require "spec_helper"
+load "exe/plurimath"
+
+RSpec.describe PlurimathCLI do
+
+  describe ".convert" do
+    context "contains no argument" do
+      let(:options) { ["convert"] }
+
+      it "should match exit status" do
+        expect do
+          described_class.start(options)
+        end.to raise_error(SystemExit) { |error| expect(error.status).to eq(1) }
+      end
+    end
+
+    context "contains input argument only" do
+      let(:mathml) do
+        <<~MATHML
+          <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+            <mstyle displaystyle="true">
+              <mo>&#x2211;</mo>
+            </mstyle>
+          </math>
+        MATHML
+      end
+
+      it "should match mathml output string" do
+        expect do
+          described_class.start(["convert", "-i", "sum"])
+        end.to output(mathml).to_stdout
+      end
+    end
+
+    context "contains input, input language, and output language arguments" do
+      let(:mathml) do
+        <<~MATHML
+          <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+            <mstyle displaystyle="true">
+              <mo>&#x2211;</mo>
+            </mstyle>
+          </math>
+        MATHML
+      end
+
+      it "should match mathml output string" do
+        expect do
+          described_class.start(["convert", "-i", mathml, "-f", "mathml", "-t", "latex"])
+        end.to output("\\sum\n").to_stdout
+      end
+    end
+
+    context "contains file input, input language, and output language arguments" do
+      let(:mathml) do
+        <<~MATHML
+          <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+            <mstyle displaystyle="true">
+              <msub>
+                <mfrac>
+                  <mi>s</mi>
+                  <mi>c</mi>
+                </mfrac>
+                <mi>d</mi>
+              </msub>
+            </mstyle>
+          </math>
+        MATHML
+      end
+
+      it "should match mathml output string" do
+        file_path = "spec/plurimath/fixtures/executables/power_base.tex"
+        options = ["convert", "-e", file_path, "-f", "latex", "-t", "mathml"]
+        expect{described_class.start(options)}.to output(mathml).to_stdout
+      end
+    end
+  end
+end
