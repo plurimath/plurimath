efa7809d349c93ac3af5033e4c392cddfe7e0d03
suleman-uzair <suleman@workytical.com>
2024-12-24 16:11:58 +0500

    update failing spec

--- spec/plurimath/mathml_spec.rb
+++ spec/plurimath/mathml_spec.rb
@@ -3421,34 +3421,70 @@ RSpec.describe Plurimath::Mathml do
         omml = <<~OMML
           <m:oMathPara xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:mo="http://schemas.microsoft.com/office/mac/office/2008/main" xmlns:mv="urn:schemas-microsoft-com:mac:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" xmlns:w15="http://schemas.microsoft.com/office/word/2012/wordml" xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing" xmlns:wpc="http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas" xmlns:wpg="http://schemas.microsoft.com/office/word/2010/wordprocessingGroup" xmlns:wpi="http://schemas.microsoft.com/office/word/2010/wordprocessingInk" xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape">
             <m:oMath>
-              <m:nary>
-                <m:naryPr>
-                  <m:chr m:val="∭"/>
-                  <m:limLoc m:val="undOvr"/>
-                  <m:supHide m:val="1"/>
+              <m:sSub>
+                <m:sSubPr>
                   <m:ctrlPr>
                     <w:rPr>
                       <w:rFonts w:ascii="Cambria Math" w:hAnsi="Cambria Math"/>
                       <w:i/>
                     </w:rPr>
                   </m:ctrlPr>
-                </m:naryPr>
-                <m:sub>
-                  <m:r>
-                    <m:t>F</m:t>
-                  </m:r>
-                </m:sub>
-                <m:sup>
-                  <m:r>
-                    <m:t>&#8203;</m:t>
-                  </m:r>
-                </m:sup>
+                </m:sSubPr>
                 <m:e>
                   <m:r>
-                    <m:t>C</m:t>
+                    <m:t>y</m:t>
                   </m:r>
                 </m:e>
-              </m:nary>
+                <m:sub>
+                  <m:r>
+                    <m:t>k</m:t>
+                  </m:r>
+                </m:sub>
+              </m:sSub>
+              <m:r>
+                <m:t>=</m:t>
+              </m:r>
+              <m:d>
+                <m:dPr>
+                  <m:begChr m:val="("/>
+                  <m:sepChr m:val=""/>
+                  <m:endChr m:val=")"/>
+                </m:dPr>
+                <m:e>
+                  <m:sSub>
+                    <m:sSubPr>
+                      <m:ctrlPr>
+                        <w:rPr>
+                          <w:rFonts w:ascii="Cambria Math" w:hAnsi="Cambria Math"/>
+                          <w:i/>
+                        </w:rPr>
+                      </m:ctrlPr>
+                    </m:sSubPr>
+                    <m:e>
+                      <m:r>
+                        <m:t>x</m:t>
+                      </m:r>
+                    </m:e>
+                    <m:sub>
+                      <m:r>
+                        <m:t>k</m:t>
+                      </m:r>
+                    </m:sub>
+                  </m:sSub>
+                  <m:r>
+                    <m:t>&#xb1;</m:t>
+                  </m:r>
+                  <m:r>
+                    <m:t>h</m:t>
+                  </m:r>
+                </m:e>
+              </m:d>
+              <m:r>
+                <m:t/>
+              </m:r>
+              <m:r>
+                <m:t>m</m:t>
+              </m:r>
             </m:oMath>
           </m:oMathPara>
         OMML
