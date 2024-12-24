a4c3a65d7cf08e9dc72131326b573921ddc8bbb8
suleman-uzair <suleman@workytical.com>
2024-12-24 15:17:54 +0500

    fixed crash for empty mo tag conversion to OMML

--- spec/plurimath/mathml_spec.rb
+++ spec/plurimath/mathml_spec.rb
@@ -3385,6 +3385,76 @@ RSpec.describe Plurimath::Mathml do
         expect(formula).to eq(omml)
       end
     end
+
+    context "contains empty mo example from plurimath/plurimath#318 Mathml" do
+      let(:string)  do
+        <<~MATHML
+          <math xmlns="http://www.w3.org/1998/Math/MathML">
+            <mstyle displaystyle="true">
+              <msub>
+                <mi>y</mi>
+                <mi>k</mi>
+              </msub>
+              <mrow>
+                <mo>=</mo>
+              </mrow>
+              <mrow>
+                <mo>(</mo>
+                <msub>
+                  <mi>x</mi>
+                  <mi>k</mi>
+                </msub>
+                <mrow>
+                  <mo>&#xB1;</mo>
+                </mrow>
+                <mi>h</mi>
+                <mo>)</mo>
+              </mrow>
+              <mo/>
+              <mi>m</mi>
+            </mstyle>
+          </math>
+        MATHML
+      end
+
+      it 'compares OMML string' do
+        omml = <<~OMML
+          <m:oMathPara xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:mo="http://schemas.microsoft.com/office/mac/office/2008/main" xmlns:mv="urn:schemas-microsoft-com:mac:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" xmlns:w15="http://schemas.microsoft.com/office/word/2012/wordml" xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing" xmlns:wpc="http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas" xmlns:wpg="http://schemas.microsoft.com/office/word/2010/wordprocessingGroup" xmlns:wpi="http://schemas.microsoft.com/office/word/2010/wordprocessingInk" xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape">
+            <m:oMath>
+              <m:nary>
+                <m:naryPr>
+                  <m:chr m:val="∭"/>
+                  <m:limLoc m:val="undOvr"/>
+                  <m:supHide m:val="1"/>
+                  <m:ctrlPr>
+                    <w:rPr>
+                      <w:rFonts w:ascii="Cambria Math" w:hAnsi="Cambria Math"/>
+                      <w:i/>
+                    </w:rPr>
+                  </m:ctrlPr>
+                </m:naryPr>
+                <m:sub>
+                  <m:r>
+                    <m:t>F</m:t>
+                  </m:r>
+                </m:sub>
+                <m:sup>
+                  <m:r>
+                    <m:t>&#8203;</m:t>
+                  </m:r>
+                </m:sup>
+                <m:e>
+                  <m:r>
+                    <m:t>C</m:t>
+                  </m:r>
+                </m:e>
+              </m:nary>
+            </m:oMath>
+          </m:oMathPara>
+        OMML
+        expect(formula).to eq(omml)
+      end
+    end
   end
 
   describe ".to_html" do
