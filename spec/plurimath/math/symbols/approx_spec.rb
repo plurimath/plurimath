567bf98db5a0422ba614e6981a87653079709e21
suleman-uzair <suleman@workytical.com>
2025-08-20 21:25:16 +0500

    Updated approx symbol's MathML output

--- spec/plurimath/math/symbols/approx_spec.rb
+++ spec/plurimath/math/symbols/approx_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe Plurimath::Math::Symbols::Approx do
 
       it "matches MathML string" do
         string = dump_ox_nodes(klass.to_mathml_without_math_tag(false)).strip
-        expect(string).to eq("<mi>&#x2248;</mi>")
+        expect(string).to eq("<mo>&#x2248;</mo>")
       end
 
       it "matches HTML string" do
