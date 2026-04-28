09f87721d1c1fca40b90cf464c56f4f5d06c6364
suleman-uzair <suleman@workytical.com>
2026-04-28 20:48:00 +0500

    fix: keep source loading change scoped

--- lib/plurimath/xml_engine.rb
+++ lib/plurimath/xml_engine.rb
@@ -2,7 +2,7 @@ module Plurimath
   singleton_class.attr_accessor :xml_engine
 
   module XmlEngine
-    autoload :Oga, File.expand_path("xml_engine/oga", __dir__)
-    autoload :OxEngine, File.expand_path("xml_engine/ox_engine", __dir__)
+    autoload :Oga, "plurimath/xml_engine/oga"
+    autoload :Ox, "plurimath/xml_engine/ox"
   end
 end
