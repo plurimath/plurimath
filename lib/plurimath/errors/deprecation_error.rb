758e7a972429ddf1f7cec6a34029ec9b9a6c58bc
suleman-uzair <suleman@workytical.com>
2026-05-12 17:29:04 +0500

    Tighten configuration fallback behavior

--- lib/plurimath/errors/deprecation_error.rb
+++ lib/plurimath/errors/deprecation_error.rb
@@ -3,23 +3,28 @@
 module Plurimath
   class DeprecationError < Error
     def initialize(feature:, message: nil, replacement: nil, since: nil, remove_in: nil)
-      @feature = feature
-      @message = message
-      @replacement = replacement
-      @since = since
-      @remove_in = remove_in
-      super(message)
+      super(
+        build_message(
+          feature: feature,
+          message: message,
+          replacement: replacement,
+          since: since,
+          remove_in: remove_in,
+        ),
+      )
     end
 
-    def message
-      parts = ["[plurimath][DEPRECATION] #{@feature} is deprecated"]
-      parts << "since #{@since}" if @since
-      parts << "and will be removed in #{@remove_in}" if @remove_in
-      parts << "Use #{@replacement} instead" if @replacement
-      parts << @message if @message
-      message = parts.join(". ")
-      message << "."
-      message
+    private
+
+    def build_message(feature:, message:, replacement:, since:, remove_in:)
+      deprecation = "[plurimath][DEPRECATION] #{feature} is deprecated"
+      deprecation = "#{deprecation} since #{since}" if since
+      deprecation = "#{deprecation} and will be removed in #{remove_in}" if remove_in
+
+      parts = [deprecation]
+      parts << "Use #{replacement} instead" if replacement
+      parts << message if message
+      "#{parts.join('. ')}."
     end
   end
 end
